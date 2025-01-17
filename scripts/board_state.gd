d71987f917fa319ef1716a3db88b66f09d5c318b
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2025-01-18 01:34:29 +0400

    Allow redealing draw pile

--- scripts/board_state.gd
+++ scripts/board_state.gd
@@ -1,6 +1,9 @@
 # board_state.gd
 extends Node2D
 
+signal on_drawpile_empty
+signal on_drawpile_full
+
 # The card scene to be duplicated for all cards in the deck
 var card_scene: PackedScene = preload("res://scenes/card.tscn")
 var drawpile_z_index := 0
@@ -30,13 +33,13 @@ var stock_pile: Array[Card] = []
 # Array of card texture names
 var card_names: Array[String] = [
 	"card_hearts_A", "card_hearts_02", "card_hearts_03", "card_hearts_04", "card_hearts_05", "card_hearts_06",
-	"card_hearts_07", "card_hearts_08", "card_hearts_09", "card_hearts_10", "card_hearts_J", "card_hearts_Q", "card_hearts_K",
-	"card_diamonds_A", "card_diamonds_02", "card_diamonds_03", "card_diamonds_04", "card_diamonds_05", "card_diamonds_06",
-	"card_diamonds_07", "card_diamonds_08", "card_diamonds_09", "card_diamonds_10", "card_diamonds_J", "card_diamonds_Q", "card_diamonds_K",
-	"card_clubs_A", "card_clubs_02", "card_clubs_03", "card_clubs_04", "card_clubs_05", "card_clubs_06", "card_clubs_07",
-	"card_clubs_08", "card_clubs_09", "card_clubs_10", "card_clubs_J", "card_clubs_Q", "card_clubs_K",
-	"card_spades_A", "card_spades_02", "card_spades_03", "card_spades_04", "card_spades_05", "card_spades_06",
-	"card_spades_07", "card_spades_08", "card_spades_09", "card_spades_10", "card_spades_J", "card_spades_Q", "card_spades_K"
+	#"card_hearts_07", "card_hearts_08", "card_hearts_09", "card_hearts_10", "card_hearts_J", "card_hearts_Q", "card_hearts_K",
+	#"card_diamonds_A", "card_diamonds_02", "card_diamonds_03", "card_diamonds_04", "card_diamonds_05", "card_diamonds_06",
+	#"card_diamonds_07", "card_diamonds_08", "card_diamonds_09", "card_diamonds_10", "card_diamonds_J", "card_diamonds_Q", "card_diamonds_K",
+	#"card_clubs_A", "card_clubs_02", "card_clubs_03", "card_clubs_04", "card_clubs_05", "card_clubs_06", "card_clubs_07",
+	#"card_clubs_08", "card_clubs_09", "card_clubs_10", "card_clubs_J", "card_clubs_Q", "card_clubs_K",
+	#"card_spades_A", "card_spades_02", "card_spades_03", "card_spades_04", "card_spades_05", "card_spades_06",
+	#"card_spades_07", "card_spades_08", "card_spades_09", "card_spades_10", "card_spades_J", "card_spades_Q", "card_spades_K"
 ]
 
 func _ready() -> void:
@@ -48,6 +51,7 @@ func initialize_deck() -> void:
 		# Add card_instance to the scene tree
 		add_child(card_instance)
 		card_instance.set_card_properties(texture_name)
+		card_instance.name = texture_name
 		
 		all_cards.append(card_instance)
 		draw_pile.append(card_instance)
@@ -60,8 +64,34 @@ func shuffle_deck(deck: Array) -> void:
 	deck.shuffle()
 
 func draw_card() -> Card:
-	var drawn_card = draw_pile.pop_back() as Card
-	stock_pile.append(drawn_card)
-	drawn_card.z_index = drawpile_z_index
-	drawpile_z_index += 1
-	return drawn_card
+	if draw_pile.size() == 0:
+		restock()
+		return
+	else:
+		on_drawpile_full.emit()
+		var drawn_card: Card = draw_pile.pop_back() as Card
+		stock_pile.append(drawn_card)
+		drawn_card.z_index = drawpile_z_index
+		drawn_card.show_front()
+		drawpile_z_index += 1
+		return drawn_card
+	
+func restock() -> void:
+	print("Before restock:")
+	print("Stock pile size:", stock_pile.size())
+	print("Draw pile size:", draw_pile.size())
+
+	on_drawpile_empty.emit()
+	
+	draw_pile = stock_pile.duplicate()
+	draw_pile.reverse()
+	stock_pile.clear()
+	
+	for card in draw_pile:
+		card.z_index = 0
+		card.show_back()
+		card.position = Vector2(-100, -100)
+
+	print("After restock:")
+	print("Stock pile size:", stock_pile.size())
+	print("Draw pile size:", draw_pile.size())
--- scripts/draw_pile.gd
+++ scripts/draw_pile.gd
@@ -1,8 +1,24 @@
 extends Area2D
 
 @onready var stockpile: Marker2D = %Stockpile
+@onready var pile_full: Sprite2D = %PileFull
+@onready var pile_empty: Sprite2D = %PileEmpty
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+
+func _ready() -> void:
+	BoardState.on_drawpile_empty.connect(_on_drawpile_empty)
+	BoardState.on_drawpile_full.connect(_on_drawpile_full)
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
-		var drawn_card = BoardState.draw_card()
-		drawn_card.position = stockpile.global_position
+		var drawn_card: Card = BoardState.draw_card()
+		if drawn_card:
+			drawn_card.position = stockpile.global_position
+
+func _on_drawpile_empty() -> void:
+	pile_empty.show()
+	pile_full.hide()
+
+func _on_drawpile_full() -> void:
+	pile_empty.hide()
+	pile_full.show()
